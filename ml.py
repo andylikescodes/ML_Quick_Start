15eade487371056406d798f0329d12a083ab79b2
Andy Liang <andylikescodes@gmail.com>
2019-04-20 02:12:54 -0700

    added cnn quick start

--- ml.py
+++ ml.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 from sklearn.datasets import make_regression
 from matplotlib.colors import ListedColormap
@@ -21,46 +22,169 @@ from sklearn.ensemble import GradientBoostingClassifier
 # Keras and Stuff
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.optimizers import RMSprop, Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Conv2D, MaxPooling2D
+
+
 
 # Keras model
-class KerasBase:
-    def __init__(self, input_size, num_classes = 3):
-        self.num_classes = num_classes
-        self.model = Sequential()
-        self.model.add(Dense(16, activation='relu', input_shape=(input_size,)))
-        self.model.add(Dense(8, activation='relu'))
-        self.model.add(Dense(4, activation='relu'))
-        self.model.add(Dense(self.num_classes, activation='softmax'))
-    
-    def fit(self, X_train, y_train, verbose=0, summary=False, batch_size = 64, epochs = 100, validation_split=0.2):
-        
-        y_train = keras.utils.to_categorical(y_train, self.num_classes)
-        self.model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
-              metrics=['accuracy'])
-        
-        if summary == True:
-            self.model.summary()
-        
-        history = self.model.fit(X_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=verbose,
-                    validation_split=validation_split)
-        return self
-        
-    def score(self, X_test, y_test, verbose=0):
-        # convert class vectors to binary class matrices
 
-        y_test = keras.utils.to_categorical(y_test, self.num_classes)
-        score = self.model.evaluate(X_test, y_test, verbose=verbose)
-        return score
-    
-    def predict(self, X_test):
-        pred = self.model.predict_classes(X_test)
-        return pred
+def CNN_quick_start(X, y, test_size, batch_size=32, num_classes=2, epochs=100, data_augmentation=True,
+                    num_predictions=20, verbose=1):
+
+    # The data, split between train and test sets:
+    x_train, x_test, y_train, y_test = train_test_split(X, y, 
+                                                        test_size=0.3, random_state=42)
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+
+    # Convert class vectors to binary class matrices.
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='valid',
+                     input_shape=x_train.shape[1:], data_format='channels_last'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3), padding='valid'))
+    model.add(Activation('relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='valid'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3), padding='valid'))
+    model.add(Activation('relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+
+    # initiate RMSprop optimizer
+    opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+
+    # Let's train the model using RMSprop
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=opt,
+                  metrics=['accuracy'])
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    # x_train /= 255
+    # x_test /= 255
+
+    if not data_augmentation:
+        print('Not using data augmentation.')
+        model.fit(x_train, y_train,
+                  batch_size=batch_size,
+                  epochs=epochs,
+                  validation_data=(x_test, y_test),
+                  shuffle=True,
+                  verbose=verbose)
+    else:
+        print('Using real-time data augmentation.')
+        # This will do preprocessing and realtime data augmentation:
+        datagen = ImageDataGenerator(
+            featurewise_center=False,  # set input mean to 0 over the dataset
+            samplewise_center=False,  # set each sample mean to 0
+            featurewise_std_normalization=False,  # divide inputs by std of the dataset
+            samplewise_std_normalization=False,  # divide each input by its std
+            zca_whitening=False,  # apply ZCA whitening
+            zca_epsilon=1e-06,  # epsilon for ZCA whitening
+            rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+            # randomly shift images horizontally (fraction of total width)
+            width_shift_range=0.1,
+            # randomly shift images vertically (fraction of total height)
+            height_shift_range=0.1,
+            shear_range=0.,  # set range for random shear
+            zoom_range=0.,  # set range for random zoom
+            channel_shift_range=0.,  # set range for random channel shifts
+            # set mode for filling points outside the input boundaries
+            fill_mode='nearest',
+            cval=0.,  # value used for fill_mode = "constant"
+            horizontal_flip=True,  # randomly flip images
+            vertical_flip=False,  # randomly flip images
+            # set rescaling factor (applied before any other transformation)
+            rescale=None,
+            # set function that will be applied on each input
+            preprocessing_function=None,
+            # image data format, either "channels_first" or "channels_last"
+            data_format=None,
+            # fraction of images reserved for validation (strictly between 0 and 1)
+            validation_split=0.0)
+
+        # Compute quantities required for feature-wise normalization
+        # (std, mean, and principal components if ZCA whitening is applied).
+        datagen.fit(x_train)
+
+        # Fit the model on the batches generated by datagen.flow().
+        print('Start Fitting Model...')
+        model.fit_generator(datagen.flow(x_train, y_train,
+                                         batch_size=batch_size),
+                            epochs=epochs,
+                            steps_per_epoch=len(x_train)/batch_size,
+                            validation_data=(x_test, y_test),
+                            workers=4,
+                            verbose=verbose)
+
+    # Save model and weights
+    # if not os.path.isdir(save_dir):
+    #     os.makedirs(save_dir)
+    # model_path = os.path.join(save_dir, model_name)
+    # model.save(model_path)
+    # print('Saved trained model at %s ' % model_path)
+
+    # Score trained model.
+    scores = model.evaluate(x_test, y_test, verbose=1)
+    print('Test loss:', scores[0])
+    print('Test accuracy:', scores[1])
+    return scores[1]
+
+
+    class KerasBase:
+        def __init__(self, input_size, num_classes = 3):
+            self.num_classes = num_classes
+            self.model = Sequential()
+            self.model.add(Dense(16, activation='relu', input_shape=(input_size,)))
+            self.model.add(Dense(8, activation='relu'))
+            self.model.add(Dense(4, activation='relu'))
+            self.model.add(Dense(self.num_classes, activation='softmax'))
+        
+        def fit(self, X_train, y_train, verbose=0, summary=False, batch_size = 64, epochs = 100, validation_split=0.2):
+            
+            y_train = keras.utils.to_categorical(y_train, self.num_classes)
+            self.model.compile(loss='categorical_crossentropy',
+                  optimizer=RMSprop(),
+                  metrics=['accuracy'])
+            
+            if summary == True:
+                self.model.summary()
+            
+            history = self.model.fit(X_train, y_train,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        verbose=verbose,
+                        validation_split=validation_split)
+            return self
+            
+        def score(self, X_test, y_test, verbose=0):
+            # convert class vectors to binary class matrices
+
+            y_test = keras.utils.to_categorical(y_test, self.num_classes)
+            score = self.model.evaluate(X_test, y_test, verbose=verbose)
+            return score
+        
+        def predict(self, X_test):
+            pred = self.model.predict_classes(X_test)
+            return pred
     
 
 
